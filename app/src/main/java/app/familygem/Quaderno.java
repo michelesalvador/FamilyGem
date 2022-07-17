8acf07df75c3754a4dd85f31f048fff1325f58a9
Michele Salvador <michelesalvador@iol.it>
2022-07-17 14:42:29 +0200

    Quaderno refactored

--- app/src/main/java/app/familygem/Quaderno.java
+++ app/src/main/java/app/familygem/Quaderno.java
@@ -23,46 +23,46 @@ import app.familygem.visita.TrovaPila;
 import static app.familygem.Global.gc;
 
 public class Quaderno extends Fragment {
-	
+
 	@Override
-	public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle bandolo ) {
-		boolean scegliNota = getActivity().getIntent().getBooleanExtra("quadernoScegliNota",false );
-		View vista = inflater.inflate( R.layout.magazzino, container, false );
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
+		boolean scegliNota = getActivity().getIntent().getBooleanExtra("quadernoScegliNota", false);
+		View view = inflater.inflate(R.layout.magazzino, container, false);
 		if( gc != null ) {
-			LinearLayout scatola = vista.findViewById( R.id.magazzino_scatola );
+			LinearLayout layout = view.findViewById(R.id.magazzino_scatola);
 			// Note condivise
 			List<Note> listaNoteCondivise = gc.getNotes();
 			if( !listaNoteCondivise.isEmpty() ) {
 				if( !scegliNota ) {
-					View tit = inflater.inflate( R.layout.titoletto, scatola, true );
-					String txt = listaNoteCondivise.size() +" "+
+					View tit = inflater.inflate(R.layout.titoletto, layout, true);
+					String txt = listaNoteCondivise.size() + " " +
 							getString(listaNoteCondivise.size()==1 ? R.string.shared_note : R.string.shared_notes).toLowerCase();
 					((TextView)tit.findViewById(R.id.titolo_testo)).setText( txt );
 				}
 				for( Note not : listaNoteCondivise )
-					registerForContextMenu( mettiNota(scatola,not) );
+					registerForContextMenu( placeNote(layout,not) );
 			}
 			// Note inlinea
 			ListaNote visitaNote = new ListaNote();
 			if( !scegliNota ) {
 				gc.accept( visitaNote );
 				if( !visitaNote.listaNote.isEmpty() ) {
-					View tit = inflater.inflate( R.layout.titoletto, scatola, false );
-					String txt = visitaNote.listaNote.size() +" "+
-							getString(visitaNote.listaNote.size()==1 ? R.string.simple_note : R.string.simple_notes).toLowerCase();
-					((TextView)tit.findViewById(R.id.titolo_testo)).setText( txt );
+					View tit = inflater.inflate(R.layout.titoletto, layout, false);
+					String txt = visitaNote.listaNote.size() + " " +
+							getString(visitaNote.listaNote.size() == 1 ? R.string.simple_note : R.string.simple_notes).toLowerCase();
+					((TextView)tit.findViewById(R.id.titolo_testo)).setText(txt);
 					//((TextView)tit.findViewById(R.id.titolo_numero)).setText( String.valueOf(visitaNote.listaNote.size()) );
-					scatola.addView( tit );
+					layout.addView(tit);
 					for( Object nota : visitaNote.listaNote )
-						registerForContextMenu( mettiNota(scatola,(Note)nota) );
+						registerForContextMenu(placeNote(layout, (Note)nota));
 				}
 			}
 			int totaleNote = listaNoteCondivise.size() + visitaNote.listaNote.size();
-			((AppCompatActivity)getActivity()).getSupportActionBar().setTitle( totaleNote + " "
-					+ getString(totaleNote==1 ? R.string.note : R.string.notes).toLowerCase() );
-			vista.findViewById( R.id.fab ).setOnClickListener( v -> nuovaNota( getContext(), null ) );
+			((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(totaleNote + " "
+					+ getString(totaleNote == 1 ? R.string.note : R.string.notes).toLowerCase());
+			view.findViewById(R.id.fab).setOnClickListener(v -> nuovaNota(getContext(), null));
 		}
-		return vista;
+		return view;
 	}
 
 	// Andandosene dall'attività senza aver scelto una nota condivisa resetta l'extra
@@ -72,62 +72,62 @@ public class Quaderno extends Fragment {
 		getActivity().getIntent().removeExtra("quadernoScegliNota");
 	}
 
-	View mettiNota( final LinearLayout scatola, final Note nota ) {
-		View vistaNota = LayoutInflater.from(scatola.getContext()).inflate( R.layout.quaderno_pezzo, scatola, false );
-		scatola.addView( vistaNota );
-		String testo = nota.getValue();
-		((TextView)vistaNota.findViewById( R.id.nota_testo )).setText( testo );
-		TextView vistaCita = vistaNota.findViewById( R.id.nota_citazioni );
-		if( nota.getId() == null )
-			vistaCita.setVisibility( View.GONE );
+	View placeNote(final LinearLayout layout, final Note note) {
+		View noteView = LayoutInflater.from(layout.getContext()).inflate(R.layout.quaderno_pezzo, layout, false);
+		layout.addView(noteView);
+		String text = note.getValue().replace("@@","@");
+		((TextView)noteView.findViewById(R.id.nota_testo)).setText(text);
+		TextView vistaCita = noteView.findViewById(R.id.nota_citazioni);
+		if( note.getId() == null )
+			vistaCita.setVisibility(View.GONE);
 		else {
-			RiferimentiNota contaUso = new RiferimentiNota( gc, nota.getId(), false );
-			vistaCita.setText( String.valueOf(contaUso.tot) );
+			RiferimentiNota contaUso = new RiferimentiNota(gc, note.getId(), false);
+			vistaCita.setText(String.valueOf(contaUso.tot));
 		}
-		vistaNota.setOnClickListener( v -> {
+		noteView.setOnClickListener(v -> {
 			// Restituisce l'id di una nota a Individuo e Dettaglio
-			if( getActivity().getIntent().getBooleanExtra("quadernoScegliNota",false) ) {
+			if( getActivity().getIntent().getBooleanExtra("quadernoScegliNota", false) ) {
 				Intent intento = new Intent();
-				intento.putExtra( "idNota", nota.getId() );
-				getActivity().setResult( AppCompatActivity.RESULT_OK, intento );
+				intento.putExtra("idNota", note.getId());
+				getActivity().setResult(AppCompatActivity.RESULT_OK, intento);
 				getActivity().finish();
 			} else { // Apre il dettaglio della nota
-				Intent intento = new Intent( scatola.getContext(), Nota.class );
-				if( nota.getId() != null ) { // Nota condivisa
-					Memoria.setPrimo( nota );
+				Intent intento = new Intent(layout.getContext(), Nota.class);
+				if( note.getId() != null ) { // Nota condivisa
+					Memoria.setPrimo(note);
 				} else { // Nota semplice
-					new TrovaPila( gc, nota );
-					intento.putExtra( "daQuaderno", true );
+					new TrovaPila(gc, note);
+					intento.putExtra("daQuaderno", true);
 				}
-				scatola.getContext().startActivity( intento );
+				layout.getContext().startActivity(intento);
 			}
 		});
-		vistaNota.setTag( nota );	// per il menu contestuale Elimina
-		return vistaNota;
+		noteView.setTag(note); // per il menu contestuale Elimina
+		return noteView;
 	}
 
 	// Crea una nuova nota condivisa, attaccata a un contenitore oppure slegata
-	static void nuovaNota( Context contesto, Object contenitore ){
+	static void nuovaNota(Context contesto, Object contenitore) {
 		Note notaNova = new Note();
-		String id = U.nuovoId( gc, Note.class );
-		notaNova.setId( id );
-		notaNova.setValue( "" );
-		gc.addNote( notaNova );
+		String id = U.nuovoId(gc, Note.class);
+		notaNova.setId(id);
+		notaNova.setValue("");
+		gc.addNote(notaNova);
 		if( contenitore != null ) {
 			NoteRef refNota = new NoteRef();
-			refNota.setRef( id );
-			((NoteContainer)contenitore).addNoteRef( refNota );
+			refNota.setRef(id);
+			((NoteContainer)contenitore).addNoteRef(refNota);
 		}
-		U.salvaJson( true, notaNova );
-		Memoria.setPrimo( notaNova );
-		contesto.startActivity( new Intent( contesto, Nota.class ) );
+		U.salvaJson(true, notaNova);
+		Memoria.setPrimo(notaNova);
+		contesto.startActivity(new Intent(contesto, Nota.class));
 	}
 
 	private Note nota;
 	@Override
-	public void onCreateContextMenu( ContextMenu menu, View vista, ContextMenu.ContextMenuInfo info ) {
-		nota = (Note) vista.getTag();
-		menu.add(0, 0, 0, R.string.delete );
+	public void onCreateContextMenu(ContextMenu menu, View vista, ContextMenu.ContextMenuInfo info) {
+		nota = (Note)vista.getTag();
+		menu.add(0, 0, 0, R.string.delete);
 	}
 	@Override
 	public boolean onContextItemSelected( MenuItem item ) {
