8e8ec4c53f0dff77b83403f39d6dacca1b980d20
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-04 02:14:38 -0400

    Translate CountSourceCitation

--- app/src/main/java/app/familygem/visitor/CountSourceCitation.java
+++ app/src/main/java/app/familygem/visitor/CountSourceCitation.java
@@ -1,7 +1,3 @@
-// Contatore delle citazioni di una fonte
-// Servirebbe per sostituire U.quanteCitazioni(Source) in Biblioteca
-// è più preciso nella conta cioè non gli sfugge nulla, ma è quattro volte più lento
-
 package app.familygem.visitor;
 
 import org.folg.gedcom.model.EventFact;
@@ -12,9 +8,18 @@ import org.folg.gedcom.model.Person;
 import org.folg.gedcom.model.SourceCitation;
 import org.folg.gedcom.model.Visitor;
 
+/**
+ * Counter of citations of a source
+ * It would be used to replace U.quanteQuante (Source) in the Library
+ * is more accurate in counting, that is, nothing escapes him, but is four times slower.
+ *
+ * // Contatore delle citazioni di una fonte
+ * // Servirebbe per sostituire U.quanteCitazioni(Source) in Biblioteca
+ * // è più preciso nella conta cioè non gli sfugge nulla, ma è quattro volte più lento
+ * */
 public class CountSourceCitation extends Visitor {
 
-	public int quante = 0;
+	public int count = 0;
 	String id;
 
 	CountSourceCitation(String id ){
@@ -24,36 +29,36 @@ public class CountSourceCitation extends Visitor {
 	@Override
 	public boolean visit( Person p ) {
 		for( SourceCitation c : p.getSourceCitations() )
-			if( c.getRef() != null ) // necessario perché le note-fonti non hanno Ref alla fonte
-				if( c.getRef().equals(id) ) quante++;
+			if( c.getRef() != null ) // required because source-notes have no Ref at source
+				if( c.getRef().equals(id) ) count++;
 		return true;
 	}
 	@Override
 	public boolean visit( Family f ) {
 		for( SourceCitation c : f.getSourceCitations() )
 			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) quante++;
+				if( c.getRef().equals(id) ) count++;
 		return true;
 	}
 	@Override
 	public boolean visit( Name n ) {
 		for( SourceCitation c : n.getSourceCitations() )
 			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) quante++;
+				if( c.getRef().equals(id) ) count++;
 		return true;
 	}
 	@Override
 	public boolean visit( EventFact e ) {
 		for( SourceCitation c : e.getSourceCitations() )
 			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) quante++;
+				if( c.getRef().equals(id) ) count++;
 		return true;
 	}
 	@Override
 	public boolean visit( Note n ) {
 		for( SourceCitation c : n.getSourceCitations() )
 			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) quante++;
+				if( c.getRef().equals(id) ) count++;
 		return true;
 	}
 }
