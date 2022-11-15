bba67a5809e8734fc8c3e379fc9f3142e72921da
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-14 23:25:06 -0500

    Translate PlaceFinderTextView

--- app/src/main/java/app/familygem/PlaceFinderTextView.java
+++ app/src/main/java/app/familygem/PlaceFinderTextView.java
@@ -1,5 +1,3 @@
-// Classe di servizio per suggerire i nomi dei luoghi formattati in stile Gedcom grazie a GeoNames
-
 package app.familygem;
 
 import android.content.Context;
@@ -18,14 +16,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * TextView that suggests Gedcom-style place names using GeoNames
+ * */
 public class PlaceFinderTextView extends AppCompatAutoCompleteTextView {
 
 	ToponymSearchCriteria searchCriteria;
 
-	public PlaceFinderTextView(Context contesto, AttributeSet as ) {
-		super( contesto, as );
-		AdattatoreLista adattatoreLista = new AdattatoreLista( contesto, android.R.layout.simple_spinner_dropdown_item );
-		setAdapter(adattatoreLista);
+	public PlaceFinderTextView(Context context, AttributeSet as) {
+		super( context, as );
+		ListAdapter listAdapter = new ListAdapter( context, android.R.layout.simple_spinner_dropdown_item );
+		setAdapter(listAdapter);
 		setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
 		//setThreshold(2);
 
@@ -35,15 +36,15 @@ public class PlaceFinderTextView extends AppCompatAutoCompleteTextView {
 		searchCriteria.setLanguage(Locale.getDefault().getLanguage()); // en, es, it...
 		searchCriteria.setStyle(Style.FULL);
 		searchCriteria.setMaxRows(3);
-		//searchCriteria.setFuzzy(0.9); // No con setNameStartsWith
-		//searchCriteria.setFeatureClass( FeatureClass.A ); // o uno o l'altro
+		//searchCriteria.setFuzzy(0.9); // Not with setNameStartsWith
+		//searchCriteria.setFeatureClass( FeatureClass.A ); // either one or the other
 		//searchCriteria.setFeatureClass( FeatureClass.P );
 	}
 
-	class AdattatoreLista extends ArrayAdapter<String> implements Filterable {
+	class ListAdapter extends ArrayAdapter<String> implements Filterable {
 		List<String> places;
-		AdattatoreLista( Context contesto, int pezzo ) {
-			super( contesto, pezzo );
+		ListAdapter(Context context, int piece ) {
+			super( context, piece );
 			places = new ArrayList<>();
 		}
 		@Override
@@ -52,7 +53,7 @@ public class PlaceFinderTextView extends AppCompatAutoCompleteTextView {
 		}
 		@Override
 		public String getItem(int index) {
-			if( places.size() > 0 && index < places.size() ) // Evita IndexOutOfBoundsException
+			if( places.size() > 0 && index < places.size() ) // Avoid IndexOutOfBoundsException
 				return places.get(index);
 			return "";
 		}
@@ -69,17 +70,17 @@ public class PlaceFinderTextView extends AppCompatAutoCompleteTextView {
 							ToponymSearchResult searchResult = WebService.search(searchCriteria);
 							places.clear();
 							for( Toponym topo : searchResult.getToponyms() ) {
-								String str = topo.getName(); // Toponimo
+								String str = topo.getName(); // Toponym
 								if(topo.getAdminName4() != null && !topo.getAdminName4().equals(str))
-									str += ", " + topo.getAdminName4(); // Paese
+									str += ", " + topo.getAdminName4(); // Village
 								if(topo.getAdminName3() != null && !str.contains(topo.getAdminName3()))
-									str += ", " + topo.getAdminName3(); // Comune
+									str += ", " + topo.getAdminName3(); // municipality
 								if(!topo.getAdminName2().isEmpty() && !str.contains(topo.getAdminName2()))
-									str += ", " + topo.getAdminName2(); // Provincia
+									str += ", " + topo.getAdminName2(); // Province/district
 								if(!str.contains(topo.getAdminName1()))
-									str += ", " + topo.getAdminName1(); // Regione
+									str += ", " + topo.getAdminName1(); // Region
 								if(!str.contains(topo.getCountryName()))
-									str += ", " + topo.getCountryName(); // Nazione
+									str += ", " + topo.getCountryName(); // Country
 								if( str != null && !places.contains( str ) ) // Avoid null and duplicates
 									places.add( str );
 							}
