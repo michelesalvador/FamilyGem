ca64a19ba67c79984ba6b4b197b04194bcda6e25
Michele Salvador <michelesalvador@iol.it>
2026-09-06 17:41:21 +0200

    Manage language country

--- app/src/main/java/app/familygem/SettingsActivity.kt
+++ app/src/main/java/app/familygem/SettingsActivity.kt
@@ -31,7 +31,7 @@ class SettingsActivity : BaseActivity(R.string.settings) {
             if (firstLocale != null) {
                 for (i in 1 until languages.size) {
                     val language = languages[i]
-                    if (firstLocale.toString().startsWith(language.code!!)) return language
+                    if (firstLocale.toString().startsWith(language.isoCode)) return language
                 }
             }
             return languages[0]
@@ -94,8 +94,12 @@ class SettingsActivity : BaseActivity(R.string.settings) {
         val xpp: XmlPullParser = resources.getXml(R.xml.locales_config)
         while (xpp.eventType != XmlPullParser.END_DOCUMENT) {
             if (xpp.eventType == XmlPullParser.START_TAG && xpp.name == "locale") {
-                val percent = xpp.getAttributeValue(null, "percent")
-                languages.add(Language(xpp.getAttributeValue(0), percent?.toInt() ?: 100))
+                languages.add(
+                    Language(
+                        xpp.getAttributeValue(0),
+                        xpp.getAttributeValue(null, "percent")?.toInt() ?: 100
+                    )
+                )
             }
             xpp.next()
         }
@@ -133,9 +137,7 @@ class SettingsActivity : BaseActivity(R.string.settings) {
     }
 
     private fun isExactAlarmsGranted(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            getSystemService<AlarmManager>()!!.canScheduleExactAlarms()
-        } else true
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.S || getSystemService<AlarmManager>()!!.canScheduleExactAlarms()
     }
 
     override fun onResume() {
@@ -146,6 +148,10 @@ class SettingsActivity : BaseActivity(R.string.settings) {
     }
 
     inner class Language(val code: String?, private val percent: Int) : Comparable<Language> {
+
+        val isoCode: String
+            get() = code?.replace('-', '_') ?: ""
+
         override fun toString(): String {
             return if (code == null) {
                 // Returns the string "System language" on the device locale, not on the app locale
@@ -155,7 +161,7 @@ class SettingsActivity : BaseActivity(R.string.settings) {
                 configuration.setLocale(deviceLocale)
                 createConfigurationContext(configuration).getText(R.string.system_language).toString()
             } else {
-                val locale = Locale(code)
+                val locale = Locale.forLanguageTag(code)
                 var txt = locale.getDisplayLanguage(locale)
                 txt = txt.substring(0, 1).uppercase() + txt.substring(1)
                 if (percent < 100) txt += " ($percent%)"
