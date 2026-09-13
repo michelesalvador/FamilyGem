f9dcbc869f6b14127b91442cd7fde9d10314a8ca
Michele Salvador <michelesalvador@iol.it>
2026-09-13 17:51:52 +0200

    Refactor address composition

--- app/src/main/java/app/familygem/util/AddressUtil.kt
+++ app/src/main/java/app/familygem/util/AddressUtil.kt
@@ -3,23 +3,19 @@ package app.familygem.util
 import org.folg.gedcom.model.Address
 
 /**
- * Writes the address concatenating all fields.
+ * Writes the address concatenating all available fields.
  * @param singleLine Comma-separated single line or multi-line
  */
 fun Address.toString(singleLine: Boolean): String {
-    var txt = ""
-    val br = if (singleLine) ", " else "\n"
-    if (value != null) txt = value + br
-    if (addressLine1 != null) txt += addressLine1 + br
-    if (addressLine2 != null) txt += addressLine2 + br
-    if (addressLine3 != null) txt += addressLine3 + br
-    if (postalCode != null || city != null || state != null) {
-        if (postalCode != null) txt += "$postalCode "
-        if (city != null) txt += "$city "
-        if (state != null) txt += state
-        txt = txt.trim() + br
-    }
-    if (country != null) txt += country
-    if (txt.endsWith(br)) txt = txt.substring(0, txt.length - br.length)
-    return txt
+    val builder = StringBuilder()
+    val delimiter = if (singleLine) ", " else "\n"
+    if (value != null) builder.append(value).append(delimiter)
+    if (addressLine1 != null) builder.append(addressLine1).append(delimiter)
+    if (addressLine2 != null) builder.append(addressLine2).append(delimiter)
+    if (addressLine3 != null) builder.append(addressLine3).append(delimiter)
+    if (postalCode != null) builder.append(postalCode).append(if (city != null) " " else if (state != null) ", " else delimiter)
+    if (city != null) builder.append(city).append(if (state != null) ", " else delimiter)
+    if (state != null) builder.append(state).append(delimiter)
+    if (country != null) builder.append(country)
+    return builder.toString().trim { it <= ' ' || it == ',' }
 }
